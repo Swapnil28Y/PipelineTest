782465e5140acc56b910b85c5aab7f20125d493b
Swapnil Yeutkar <106110395+Swapnil28Y@users.noreply.github.com>
2023-06-29 15:00:47 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Test {
+	public int check() {
+		int a=10;
+		int b=20;
+		return a+b;
+		
+	}
+	public static void main(String[] args) {
+		Test test=new Test();
+		System.out.println(test.check());
+
+	}
+
+}
